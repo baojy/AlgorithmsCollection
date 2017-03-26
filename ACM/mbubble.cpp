9baeae3dacdf6c8f65db8a23436505c494e3e93b
macbook air <macbookair@macbookdeAir.lan>
2017-03-26 11:25:02 +0800

    bubble_cpp

---
+++ ACM/mbubble.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a[]={56,96,72,45,15,60,38,83,23};
+	for(int n=0;n<8;n++)
+	{
+		int change = 0;
+		for(int i=8-1;i>=n;i--)
+		{
+			if(a[i]>a[i+1])
+			{
+				int tmp = a[i];
+				a[i] = a[i+1];
+				a[i+1] = tmp;
+				change = 1;
+			}
+
+		}
+		if(change == 0)break;
+	}
+
+	for (int i=0;i<8;++i)
+		cout<<a[i]<<" ";
+	return 0;
+}
